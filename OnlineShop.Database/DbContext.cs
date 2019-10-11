a0f98b8b721d22f4af9f2c99c5d5172b1cfafd28
Paul Tim <paveltimofeenko@gmail.com>
2019-10-11 03:20:27 +0300

    Added Product and Category Business Entities. Created Database

---
+++ OnlineShop.Database/DbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using OnlineShop.Entities;
+
+namespace OnlineShop.Database
+{
+    public class DBContext :DbContext
+    {
+        public DBContext() : base("OnlineShopConnection")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
