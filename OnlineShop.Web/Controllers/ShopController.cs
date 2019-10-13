7f52cb17b3f07e9851b7bfd2da1e5781d7ab226f
Paul Tim <paveltimofeenko@gmail.com>
2019-10-13 10:06:01 +0300

    script added to shop index

--- OnlineShop.Web/Controllers/ShopController.cs
+++ OnlineShop.Web/Controllers/ShopController.cs
@@ -27,6 +27,16 @@ namespace OnlineShop.Web.Controllers
             return View(model);
         }
 
+        public ActionResult FilterProducts(string searchTerm, int? minimumPrice, int? maximumPrice, int? categoryID, int? sortBy)
+        {
+            FilterProductsViewModel model = new FilterProductsViewModel();
+            
+            model.Products = ProductsService.Instance.SearchProducts(searchTerm, minimumPrice, maximumPrice, categoryID, sortBy);
+            
+            return PartialView(model);
+        }
+
+
 
     public ActionResult Checkout()
         {
--- OnlineShop.Web/ViewModels/ShopViewModels.cs
+++ OnlineShop.Web/ViewModels/ShopViewModels.cs
@@ -13,10 +13,14 @@ namespace OnlineShop.Web.ViewModels
         public List<int> CartProductIDs { get; set; }
     }
     public class ShopViewModel
-   {
+    {
        public int MaximumPrice { get; set; }
        public List<Category> FeaturedCategories { get; set; }
        public List<Product> Products { get; set; }
        public int? SortBy { get; set; }
-   }
+    }
+    public class FilterProductsViewModel
+    {
+        public List<Product> Products { get; set; }
+    }
 }
