65c4977be27616135e91ec30c0f03439a33dfca3
Paul Tim <paveltimofeenko@gmail.com>
2019-10-13 08:26:49 +0300

    entity and dbcontext validation

--- OnlineShop.Entities/BaseEntity.cs
+++ OnlineShop.Entities/BaseEntity.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineShop.Entities
 {
     public class BaseEntity
     {
         public int ID { get; set; }
+        [Required]
+        [MinLength(3), MaxLength(50)]
         public string Name { get; set; }
+        [MaxLength(500)]
         public string Description { get; set; }
     }
 }
--- OnlineShop.Entities/Product.cs
+++ OnlineShop.Entities/Product.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineShop.Entities
 {
@@ -11,7 +12,7 @@ namespace OnlineShop.Entities
         public virtual Category Category { get; set; }
 
         //public int CategoryID { get; set; }
-
+        [Range(1, 100000)]
         public decimal Price { get; set; }
         public string ImageURL { get; set; }
     }
